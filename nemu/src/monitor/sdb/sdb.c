01df3b0a970f4ce27e85dcdada3c417a5e3ee0bd
tracer-ysyx <tracer@ysyx.org>
2025-03-10 22:12:45 +0800

    > compile NEMU ysyx_22040000 cxy_test Linux cxy-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:12:45 up  8:49,  5 users,  load average: 0.79, 0.57, 0.43

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -83,7 +83,7 @@ static int cmd_xm(char *args) {
     int length = atoll(len);
     uint32_t value = 0x80000000;
     for (int i = 0; i * 4 < length; i++) {
-      printf("memory:0X%x, val: 0X%x\n",value + i * 4, paddr_read(value, 4));
+      printf("memory:0X%x, val: 0X%x\n",value + i * 4, paddr_read(value + i * 4, 4));
     }
   }  
   return 0;
