efd40194cf423e8726609d9fcb3623709650836e
cxy <1740907592@qq.com>
2025-09-02 22:04:05 +0800

    将 nvboard 合并到主仓库

--- nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/expr.d
+++ nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/expr.d
@@ -1,9 +1,22 @@
-/home/cs144/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/expr.o: \
- src/monitor/sdb/expr.c /home/cs144/ysyx-workbench/nemu/include/isa.h \
- /home/cs144/ysyx-workbench/nemu/src/isa/riscv32/include/isa-def.h \
- /home/cs144/ysyx-workbench/nemu/include/common.h \
- /home/cs144/ysyx-workbench/nemu/include/generated/autoconf.h \
- /home/cs144/ysyx-workbench/nemu/include/macro.h \
- /home/cs144/ysyx-workbench/nemu/include/debug.h \
- /home/cs144/ysyx-workbench/nemu/include/utils.h \
- /home/cs144/ysyx-workbench/nemu/include/memory/paddr.h
+cmd_/home/cs144/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/expr.o := unused
+
+source_/home/cs144/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/expr.o := src/monitor/sdb/expr.c
+
+deps_/home/cs144/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/expr.o := \
+  /home/cs144/ysyx-workbench/nemu/include/isa.h \
+  /home/cs144/ysyx-workbench/nemu/src/isa/riscv32/include/isa-def.h \
+    $(wildcard include/config/rve.h) \
+    $(wildcard include/config/rv64.h) \
+  /home/cs144/ysyx-workbench/nemu/include/common.h \
+    $(wildcard include/config/target/am.h) \
+    $(wildcard include/config/mbase.h) \
+    $(wildcard include/config/msize.h) \
+    $(wildcard include/config/isa64.h) \
+  /home/cs144/ysyx-workbench/nemu/include/macro.h \
+  /home/cs144/ysyx-workbench/nemu/include/debug.h \
+  /home/cs144/ysyx-workbench/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
+
+/home/cs144/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/expr.o: $(deps_/home/cs144/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/expr.o)
+
+$(deps_/home/cs144/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/expr.o):
---
+++ nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/sdb.d
@@ -0,0 +1,10 @@
+/home/cs144/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/sdb.o: \
+ src/monitor/sdb/sdb.c /home/cs144/ysyx-workbench/nemu/include/isa.h \
+ /home/cs144/ysyx-workbench/nemu/src/isa/riscv32/include/isa-def.h \
+ /home/cs144/ysyx-workbench/nemu/include/common.h \
+ /home/cs144/ysyx-workbench/nemu/include/generated/autoconf.h \
+ /home/cs144/ysyx-workbench/nemu/include/macro.h \
+ /home/cs144/ysyx-workbench/nemu/include/debug.h \
+ /home/cs144/ysyx-workbench/nemu/include/utils.h \
+ /home/cs144/ysyx-workbench/nemu/include/cpu/cpu.h src/monitor/sdb/sdb.h \
+ /home/cs144/ysyx-workbench/nemu/include/memory/paddr.h
